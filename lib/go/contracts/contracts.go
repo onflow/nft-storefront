5d4d063a9a25b9e7aad8bdbf4d8bf858e8b0a5a8
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-07-14 16:50:07 +0200

    add methods for loading

---
+++ lib/go/contracts/contracts.go
@@ -0,0 +1,52 @@
+package contracts
+
+import (
+	_ "embed"
+	"fmt"
+	"github.com/onflow/nft-storefront/internal/assets"
+	"strings"
+)
+
+//go:generate go run github.com/kevinburke/go-bindata/go-bindata -prefix ../../../contracts -o internal/assets/assets.go -pkg assets -nometadata -nomemcopy ../../../contracts
+
+const (
+	storefrontFilename                 = "NFTStorefrontV2.cdc"
+	placeholderFungibleTokenAddress    = "./utility/FungibleToken.cdc"
+	placeholderNonfungibleTokenAddress = "./utility/NonFungibleToken.cdc"
+)
+
+func NFTStorefront(fungibleTokenAddress string, nonfungibleTokenAddress string) []byte {
+	code := assets.MustAssetString(storefrontFilename)
+
+	// Replace the fungible token address
+	code = strings.ReplaceAll(
+		code,
+		placeholderFungibleTokenAddress,
+		withHexPrefix(fungibleTokenAddress),
+	)
+
+	// Replace the non-fungible token address
+	code = strings.ReplaceAll(
+		code,
+		placeholderNonfungibleTokenAddress,
+		withHexPrefix(nonfungibleTokenAddress),
+	)
+
+	return []byte(code)
+}
+
+func withHexPrefix(address string) string {
+	if address == "" {
+		return ""
+	}
+
+	if address[0:2] == "0x" {
+		return address
+	}
+
+	return fmt.Sprintf("0x%s", address)
+}
+
+func NFTStoreFrontVersion(version int) {
+
+}
