5839901ab7830fad3c609efd5d55155ba2418d10
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-07-14 17:47:18 +0200

    add version support

--- lib/go/contracts/contracts_test.go
+++ lib/go/contracts/contracts_test.go
@@ -8,9 +8,12 @@ import (
 )
 
 func Test_StorefrontContract(t *testing.T) {
-	code := NFTStorefront("0x1", "0x2")
+	code := NFTStorefront(1, "0x1", "0x2")
+	codeV2 := NFTStorefront(2, "0x1", "0x2")
 
 	assert.NotNil(t, code)
 	assert.True(t, strings.Contains(string(code), `import FungibleToken from "0x1"`))
 	assert.True(t, strings.Contains(string(code), `import NonFungibleToken from "0x2"`))
+	assert.True(t, strings.Contains(string(codeV2), `import FungibleToken from "0x1"`))
+	assert.True(t, strings.Contains(string(codeV2), `import NonFungibleToken from "0x2"`))
 }
